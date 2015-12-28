27b71e081f87d729c796aa0d1611cbe1c80e79da
Scott Crespo <screspo@highcornergroup.com>
2015-12-27 18:22:20 -0700

    broke end-to-end test into its own module. renamed int_test to unit_test

--- tests/unit_test.go
+++ tests/unit_test.go
@@ -10,29 +10,6 @@ import (
   LB "github.com/scottcrespo/eps-conduit/load-balancer"
 )
 
-func FooEndToEnd(t *testing.T) {
-
-	for i := 0; i <= 4; i = i + 1 {
-		req, err := http.NewRequest("GET", "http://localhost:8000/", nil)
-		if err != nil {
-			t.Errorf("Failed to create new request")
-		}
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Errorf("client failed to issue request")
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("failed to read response body")
-		}
-		log.Println(string(body))
-	}
-}
-
 func TestStandalone(t *testing.T) {
 
   configFile := "/etc/conduit.conf"
