d2d72b0f2cb943e05d7d737ffde51baf255f7145
Scott Crespo <screspo@highcornergroup.com>
2015-12-27 18:18:58 -0700

    end to end and standalone tests work

--- tests/int_test.go
+++ tests/int_test.go
@@ -4,10 +4,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+  "net/http/httptest"
 	"testing"
+
+  LB "github.com/scottcrespo/eps-conduit/load-balancer"
 )
 
-func TestEndToEnd(t *testing.T) {
+func FooEndToEnd(t *testing.T) {
 
 	for i := 0; i <= 4; i = i + 1 {
 		req, err := http.NewRequest("GET", "http://localhost:8000/", nil)
@@ -28,4 +31,28 @@ func TestEndToEnd(t *testing.T) {
 		}
 		log.Println(string(body))
 	}
+}
+
+func TestStandalone(t *testing.T) {
+
+  configFile := "/etc/conduit.conf"
+  // initialize the main LoadBalancer Instance using configFile and empty user input
+	lb := LB.GetLoadBalancer(configFile, &LB.LoadBalancer{})
+
+  ts := httptest.NewServer(http.HandlerFunc(lb.Handle))
+  defer ts.Close()
+  //ts.URL = "http://localhost:8000"
+
+  resp, err := http.Get(ts.URL)
+  if err != nil {
+    t.Errorf("client failed to issue request")
+  }
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    t.Errorf("failed to read response body")
+  }
+  log.Println(string(body))
+
 }
